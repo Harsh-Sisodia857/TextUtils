2ad4aa97f5342f24d16ec214805352b11e8062b3
Harsh-Sisodia857 <Harsh2012007@akgec.ac.in>
2022-04-16 00:32:32 +0530

    Adding dark mode feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './component/Navbar'
+import TextForm from './component/TextForm';
+import React, { useState } from 'react'
+import Alert from './component/Alert';
 function App() {
+  const [mode,setMode] = useState('light');
+  const [alert,setAlert] = useState(null);
+
+  
+
+
+  const handlemode = () => {
+    if (mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+  <Navbar title = "TextUtils" mode = {mode} handlemode = {handlemode}/>
+  <Alert alert = "This is alert"/>
+  <div className="container my-3" >
+  <TextForm heading = "Enter Text to Analyze below" mode = {mode} handlemode = {handlemode}/>
+  </div>
+  </div>
   );
 }
 
---
+++ src/component/TextForm.js
@@ -0,0 +1,59 @@
+import React,{useState} from 'react'
+
+export default function TextForm(props) {
+    const handleOnChange = (event) => {
+        // console.log("On change");
+        setText(event.target.value);
+    }
+    const handleUpClick = () => {
+        // console.log("Uppercase was clicked");
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+    const handleLowClick = () =>{
+      let newText = text.toLowerCase();
+      setText(newText);
+    }
+    const handleClearClick = () =>{
+      let newText = "";
+      setText(newText);
+    }
+    const Copy = () =>{
+      let text = document.getElementById('exampleFormControlTextarea1');
+      text.select();
+      navigator.clipboard.writeText(text.value);
+    }
+    const handleExtraSpace = () =>{
+      let newtext = text.split(/[ ]+/).join(" ");
+      setText(newtext);
+    }
+    const [text, setText] = useState('');
+  return (
+   <>
+<div style={{color: props.mode==='dark'?'white':'black'}}>
+<h2>{props.heading}</h2>
+  <div className="mb-3">
+  <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+  <input type="email" className="form-control" id="exampleFormControlInput1" style={{backgroundColor: props.mode ==='dark'? 'Grey' : 'white', color:props.mode === 'dark'?'white': 'black'}}  placeholder="name@example.com"/>
+</div>
+<div className="mb-3">
+  <textarea className="form-control" style={{backgroundColor: props.mode ==='dark'? 'Grey' : 'white' , color:props.mode === 'dark'?'white': 'black'}} id="exampleFormControlTextarea1" value={text} onChange={handleOnChange} rows="8"></textarea>
+</div>
+<div className="container">
+<button className="btn btn-primary mx-3" onClick={handleUpClick} >Convert to UpperCase</button>
+<button className="btn btn-primary" onClick={handleLowClick}>Convert to LowerCase</button>
+<button className="btn btn-primary mx-3" onClick={handleClearClick}>Clear Text</button>
+<button className="btn btn-primary" onClick={Copy}>Copy</button>
+<button className="btn btn-primary mx-3" onClick={handleExtraSpace}>Remove Extra Space</button>
+
+</div>
+
+<div>
+  <h1 className='my-3'>Your Text Summary</h1>
+  <p>No. of Words {text.split(" ").length}</p>
+  <p>Time to read : {text.split(" ").length * 0.008}</p>
+</div>
+</div>
+   </>
+  )
+}
